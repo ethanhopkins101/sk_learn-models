ffcfdf93317e8408d21e89629e23f6ea798975d8
ethan <ethan.hopkins101@gmail.com>
2024-11-16 20:48:54 +0100

    locally weighted regression_3

--- ml_library/linear_model.py
+++ ml_library/linear_model.py
@@ -1,4 +1,14 @@
 import numpy as np
+#function that changes the learning algorithm inputs to array-like
+def __change_type(x_train,y_train):
+    return (np.array(x_train),np.array(y_train))#returning a tuple
+#designing intercept_fitting function:
+def __fit_intercept(x_train):
+    intercept=np.ones((x_train.shape[0],1)) #column vector of ones
+    return np.hstack((intercept,x_train)) #returning the appended vector with x_train
+#arbitrary parameter initializer function:
+def __init_parameters(x_train):
+    return np.zeros((x_train.shape[1],1))#return a column vector of shape x_train[1]
 class LinearRegression:
     def __init__(self,fit_intercept=False):
         self.fit_intercept=fit_intercept
@@ -11,16 +21,17 @@ class LocallyWeightedRegression:
         self.t=t
         self.fit_intercept=fit_intercept
         self.parameters=None
-    #defining the fit function
+    #defining the fit method
     def fit(self,x_train,y_train):
         #making sure the inputs are of array type
-        x_train=np.array(x_train)
-        y_train=np.array(y_train)
+        x_train,y_train=__change_type(x_train,y_train)
+        #initializing the parameters
+        self.parameters
         #adding intercept term if specified by user
         if self.fit_intercept:
-            intercept=np.ones((x_train.shape[0],1))
-            x_train=np.hstack((intercept,x_train))
-    #defining the predict function
+            x_train=__fit_intercept(x_train)
+        
+    #defining the predict method
     def predict(self,x_test):
         pass
 
@@ -31,17 +42,15 @@ class LogisticRegression:
         self.fit_intercept=fit_intercept
         self.parameters=None
         self.learn_rate=0.05
-    # defining the fit function
+    # defining the fit method
     def fit(self,x_train,y_train):
         #making sure inputs are in array type
-        x_train=np.array(x_train)
-        y_train=np.array(y_train)
+        x_train,y_train=__change_type(x_train,y_train)
         #initializing parameters
         self.parameters=np.zeros((x_train.shape[1],1))
         #adding intercept term if specified by user
         if self.fit_intercept:
-            intercept=np.ones((x_train.shape[0],1))
-            x_train=np.hstack((intercept,x_train))
+            x_train=__fit_intercept(x_train)
         likelihood_old='inf'
         #training the model
         while(True):
@@ -53,7 +62,7 @@ class LogisticRegression:
             if likelihood_old>likelihood:
                 break
             likelihood_old=likelihood
-        
+    #defining predict method
     def predict():
         pass
 
