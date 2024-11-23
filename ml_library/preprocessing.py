d83f3e1c857795afc8559b63e6ac0ab699cc9375
ethan <ethan.hopkins101@gmail.com>
2024-11-23 20:40:16 +0100

    designing preprocessing module

---
+++ ml_library/preprocessing.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class PolynomialFeatures:
+    def __init__(self,degree):
+        self.degree=degree
+
+    #Designing the fit method
+    def fit(self,x_train,y_train):
+        pass
+    #Designing the predict method
+    def predict(self,x_test):
+        pass
+
+
+class StandardScalar:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+    def transform(self):
+        pass
