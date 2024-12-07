b4b9d9c4c83c7dc4238f310713a29d3cc4c5e591
ethan <ethan.hopkins101@gmail.com>
2024-12-07 22:02:41 +0100

    desgining the CountVectorizer class

--- ml_library/feature_extration/text.py
+++ ml_library/feature_extration/text.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matrix_operations import *
 from numpy.typing import ArrayLike
+from typing import List
 """
 text.py
 
@@ -19,10 +20,12 @@ HashingVectorizer: A vectorizer that uses the hashing trick for efficient,
 class CountVectorizer:
     
     def __init__(self)-> None : #Initializer
+
         self.dictionary=None
 
     # Defining the fit method
     def fit(self, x_train: MatrixLike | ArrayLike) -> None:
+
         temp=[] # collects all rows into one big list
         x_train=change_type(x_train) # setting x_train to np.array incase
 
@@ -37,16 +40,42 @@ class CountVectorizer:
         
         combined_content=' '.join(temp) # combines all strings in the list into one string
 
-        content_words=combined_content.split(' ') # splits content by words
+        content_words= combined_content.split(' ') # splits content by words
 
-        content_words=set(content_words) # removes all duplicates 
-        
-        self.dictionary={k:v for v,k in enumerate(content_words)}
+        content_words= set(content_words) # removes all duplicates 
+        # filling the dictionary with obtained words , with values representing current index
+        self.dictionary= {k:v for v,k in enumerate(content_words)}
     
     # Defining the Transform method
-    def transform()->np.ndarray:
-        ...
+    def transform(self, x_transform: MatrixLike | ArrayLike) -> np.ndarray:
+
+        if x_transform.shape[1] > 1: 
+            raise ValueError ('The given inputs do not satisfy the required conditions')
+            
+        x_transform= change_type(x_transform) # changing x_transform type to ndarray incase
+        
+        # Supposed transformation of (x_transform)
+        holder_matrix= np.zeros((x_transform.shape[0],len(self.dictionary)))
+
+        for i in range(x_transform.shape[0]):
+            words= x_transform[i].split(' ')
+            words= set(words) # removes duplicates
+            
+            for word in words :
+                if word in self.dictionary.keys():
+                    holder_matrix[i,self.dictionary[word]]= 1
+
+        return holder_matrix
 
     # Defining the fit_transform method
-    def fit_transform()->np.ndarray:
+    def fit_transform(self, x_transform: MatrixLike | ArrayLike) -> np.ndarray:
+        self.fit(x_transform)
+        return self.transform(x_transform)
+    
+    # Defining the 'toarray' method
+    def toarray():
         ...
+
+    # Defining the get_feature_names_out method
+    def get_feature_names_out(self) -> List[str]:
+        return self.dictionary.keys()
