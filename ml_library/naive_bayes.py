944c8142bd0ee622002c729fff3d5e11c4d1375d
ethan <ethan.hopkins101@gmail.com>
2024-12-09 19:57:44 +0100

    designing the multinomial event model class

---
+++ ml_library/naive_bayes.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+"""
+naive_bayes.py
+
+This module was constructed to mimic most implementation
+of naive bayes in machine learning, using various classes and methods
+
+Classes
+-------
+
+GaussianNB:
+MultinomialNB:
+MultivariateBernoulliNB:
+
+"""
+# Defining the GaussianNB Class
+class GaussianNB:
+    def __init__(self) -> None: #initializer
+        ...
+
+    def fit(self,x_train)
+
+# Defining the MultivariateBernoulliNB Class
+class MultivariateBernoulliNB:
+    
+    def __init__(self) -> None: #Initializer
+        ...
+
+    # Defining the fit method
+    def fit(self, x_train: ) -> None:
+        ...
+    # Defining the predict method
+
+    def predict(self, x_test ) ->None:
+        ...
+
+class MultinomialNB:
+
+    def __init__(self, ) -> None:
+        ...
