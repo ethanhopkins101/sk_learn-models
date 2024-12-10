cd45a2ba9e89d166ecd3e07e689d320553480a0a
ethan <ethan.hopkins101@gmail.com>
2024-12-10 19:33:32 +0100

    designing the naive_bayes module

--- ml_library/naive_bayes.py
+++ ml_library/naive_bayes.py
@@ -1,19 +1,42 @@
 import numpy as np
 
 """
-naive_bayes.py
+Module Name: naive_bayes
 
-This module was constructed to mimic most implementation
-of naive bayes in machine learning, using various classes and methods
+Description
+-----------
+
+    This module does implement some naive_bayes algorithms, focusing mainly on
+        text classification
+
+
+Usage:
+    Instructions on how to use the module or examples of typical use cases.
+
+Attributes:
+    - List any important constants or variables defined in the module.
 
 Classes
 -------
 
-GaussianNB:
-MultinomialNB:
-MultivariateBernoulliNB:
+    - MultinomialNB: Text classification event model, that assumes features follow
+        multinomial distribution, (features represent counts/frequencies)
+
+    - GaussianNB: Text classification model that assumes features to follow gaussian 
+        distribution (Highly effective for continuous features)
+        
+    - ComplementNB: Text classification model , that uses the complement of the
+        data distribution, to compensates for imbalanced data sets 
+
+Functions:
+    - List and describe the main functions provided by the module.
+
+Example:
+    Provide a simple example of how to use the module.
 
 """
+
+
 # Defining the GaussianNB Class
 class GaussianNB:
     def __init__(self) -> None: #initializer
