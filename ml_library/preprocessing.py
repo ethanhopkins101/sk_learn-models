e1a5585e463a22dee888a93a58899ff6c392e9d9
ethan <ethan.hopkins101@gmail.com>
2024-12-13 20:46:11 +0100

    desining the preprocessing module

--- ml_library/preprocessing.py
+++ ml_library/preprocessing.py
@@ -1,14 +1,36 @@
 import numpy as np
+from matrix_operations import *
+from numpy.typing import ArrayLike
+"""
+Module Name: preprociessing.py
 
+Description
+-----------
+    This module provides various tools/classes to preprocess most data
+
+    
+Attributes
+-----------
+    - List any important constants or variables defined in the module.
+
+Classes
+-------
+    - PolynomialFeatures: transforms input features into polynomials,
+        with specified degree
+
+    Provide a simple example of how to use the module.
+
+"""
 class PolynomialFeatures:
-    def __init__(self,degree):
-        self.degree=degree
+    def __init__(self, degree: int, fit_intercept: bool= False):
 
-    #Designing the fit method
-    def fit(self,x_train,y_train):
+        self.degree: int= degree
+
+    def fit(self, x_train: MatrixLike | ArrayLike, y_train: MatrixLike | ArrayLike):
+        
         pass
-    #Designing the predict method
-    def predict(self,x_test):
+
+    def transform(self, x_test) -> np.ndarray:
         pass
 
 
