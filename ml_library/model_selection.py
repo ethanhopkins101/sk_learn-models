f6bce7b188afab422390f702bf73fead44b5c3dc
ethan <ethan.hopkins101@gmail.com>
2024-12-16 21:21:43 +0100

    designing the train_test_split function

--- ml_library/model_selection.py
+++ ml_library/model_selection.py
@@ -1,5 +1,33 @@
 import numpy as np
+"""
+Module Name: model_selection.py
 
+Description
+-----------
+
+    This module provides various tools for model selection such as k-fold,
+     cross validation, hyper parameter tunning etc ...
+
+Usage
+-----
+    Instructions on how to use the module or examples of typical use cases.
+
+Attributes:
+    - List any important constants or variables defined in the module.
+
+Classes
+-------
+    - KFold
+    - RepeatedKFold
+    - LeavePOut
+
+Functions
+---------
+
+    - train_test_split: splits the input data into fragments of kind (x_train, y_train),
+        and (x_test, y_test)
+
+"""
 
 def tain_test_split():
     pass
