accfff5a36e668dcae6364f6d92c561b1da1a542
ethan <ethan.hopkins101@gmail.com>
2025-01-13 22:00:06 +0100

    adjusting the linear model module

--- ml_library/linear_model.py
+++ ml_library/linear_model.py
@@ -7,7 +7,7 @@ class LinearRegression:
     """
     Least Mean Square LinearRegression
     """
-    def __init__(self,fit_intercept=False):
+    def __init__(self, fit_intercept:bool= False):
         self.fit_intercept: bool= fit_intercept
         self.parameters: Optional[MatrixLike | ArrayLike]= None
 
@@ -60,7 +60,7 @@ class LinearRegression:
 
 class LocallyWeightedRegression:
 
-    def __init__(self,tunning_parameter,fit_intercept=False):
+    def __init__(self, tunning_parameter,fit_intercept: bool= False):
         self.tuning_parameter: float= tunning_parameter
         self.fit_intercept: bool= fit_intercept
         self.x_train: Optional[MatrixLike]= None
@@ -175,34 +175,34 @@ class PoissonRegression:
     
     """
 
-    #Class initializer
-    def __init__(self,fit_intercept=False,learning_rate=0.005,exit_point=np.power(10.0,-5)):
-        self.learning_rate=learning_rate
-        self.parameters=None
-        self.fit_intercept=fit_intercept #Intercept vector check
-        self.exit_point=exit_point
+    def __init__(self, fit_intercept: bool= False, learning_rate:float= 0.005,
+                 exit_point: float= np.power(10.0,-5)):
+        self.learning_rate: float= learning_rate
+        self.parameters: Optional[np.ndarray]= None
+        self.fit_intercept: bool= fit_intercept #Intercept vector check
+        self.exit_point: float= exit_point
 
-    # Defining the fit method
-    def fit(self,x_train,y_train):
-        x_train,y_train=change_type(x_train,y_train)
-        if (x_train.shape[0]!=y_train.shape[0]):
-            raise ValueError('x_train shape does not match y_train'
-                             f'{x_train.shape[0]} and {y_train.shape[0]}')
+    def fit(self, x_train: MatrixLike | ArrayLike,
+            y_train: MatrixLike | ArrayLike):
+        
+        x_train,y_train= change_type(x_train,y_train)
+        validate_inputs(x_train, y_train)
+
         if self.fit_intercept:
             x_train=fit_intercept(x_train)
-        self.parameters=init_parameters(x_train) # initializing parameters to vector of zeros
+        self.parameters= init_parameters(x_train) # initializing parameters to vector of zeros
 
         # defining the likelihood ascent :
-        self.exit_point=np.power(10.0,-5) #defining exit_point for the loop
-        gradient=np.inf
+        self.exit_point= np.power(10.0,-5) #defining exit_point for the loop
+        gradient= np.inf
 
         while(gradient>self.exit_point):
             hypothesis=np.exp(np.dot(x_train,self.parameters)) # our poisson hypothesis
             gradient=np.dot(x_train.T,y_train-hypothesis)
             self.parameters+=self.learning_rate*gradient #gradient ascent
 
-    # Defining the predict method
-    def predict(self,x_test):
+    def predict(self, x_test: MatrixLike | ArrayLike):
+
         if self.parameters is None:
             raise ValueError('Fit method was not called '
                              'Training data was not provided !')
@@ -211,3 +211,10 @@ class PoissonRegression:
         if self.fit_intercept:
             x_test=fit_intercept(x_test)
         return np.dot(x_test,self.parameters)
+
+    @property
+    def coef(self) -> float|np.ndarray:
+        return self.parameters[1:]
+    @property
+    def intercept_(self) -> float:
+        return self.parameters[0]
