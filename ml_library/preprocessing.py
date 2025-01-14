8b43aa60f06a6ad07f73eee7d7cedb1233b03528
ethan <ethan.hopkins101@gmail.com>
2025-01-14 18:07:43 +0100

    adding MinMaxScaler class to preprocessing module

--- ml_library/preprocessing.py
+++ ml_library/preprocessing.py
@@ -51,18 +51,44 @@ class StandardScaler:
             raise TypeError('Fit method was not called, parameters are not initialized ')
         return (x_transform - self.mean) / self.std
 
-    def fit_transform(self, x_train: MatrixLike | ArrayLike,
-                      x_transform: MatrixLike | ArrayLike) -> np.ndarray:
+    def fit_transform(self, x_train: MatrixLike | ArrayLike) -> np.ndarray:
+
         self.mean= x_train.mean(axis= 0)
         self.std= x_train.std(axis= 0)
 
-        return (x_transform - self.mean) / self.std
+        return (x_train - self.mean) / self.std
     
+    def inverse_transform(self, x_inverse: MatrixLike | ArrayLike) -> np.ndarray:
+        return (x_inverse * self.std) + self.mean
+    
+    def get_coef(self) -> np.ndarray:
+        
+        return np.array((self.mean,self.std))
+
 class MinMaxScaler:
 
     def __init__(self) -> None:
         self.min: Optional[np.ndarray]= None
         self.max: Optional[np.ndarray]= None
 
-    def fit(self, x_train: MatrixLike | ArrayLike):
+    def fit(self, x_train: MatrixLike | ArrayLike) -> None:
+
+        self.min= x_train.min(axis= 0)
+        self.max= x_train.max(axis= 0)
+    
+    def transform(self, x_transform: MatrixLike | ArrayLike) -> np.ndarray:
+
+        return (x_transform - self.min) / (self.max - self.min)
+
+    def fit_transform(self, x_train: MatrixLike | ArrayLike) -> np.ndarray:
         
+         self.min= x_train.min(axis= 0)
+         self.max= x_train.max(axis= 0)
+         return (x_train-self.min) / (self.max - self.min)
+    
+    def inverse_transform(self, x_inverse: MatrixLike | ArrayLike) -> np.ndarray:
+
+        return x_inverse * (self.max - self.min) + self.min
+    
+    def get_coef(self) -> np.ndarray:
+        return np.array((self.min,self.max))
