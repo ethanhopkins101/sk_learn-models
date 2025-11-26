fbc43795aa172ef0531dbd9b7740022f0a740a95
ethan <ethan.hopkins101@gmail.com>
2025-11-26 07:08:05 +0100

    gaussian discriminant analysis completed

--- otoma_library/discriminant_analysis.py
+++ otoma_library/discriminant_analysis.py
@@ -38,4 +38,37 @@ class LinearDiscriminantAnalysis:
         self.theta= np.sum(y)/len(y)
         self.mean0= np.sum(x[y == 0, :], axis= 0) / len(x[y== 0,:])
         self.mean1= np.sum(x[y == 1, :], axis= 0) / len(x[y== 1,:])
-        self.sigma= 
+        temp= x.copy()
+        for i in range(len(y)):
+            if y[i] == 0:
+                temp[i, :]= temp[i, :] - self.mean0
+            else:
+                temp[i, :]= temp[i, :] - self.mean1
+
+        self.sigma= (temp.T @ temp) / len(x)
+
+    def predict(self, xp: ArrayLike) -> np.ndarray :
+        
+        res0= []
+        res1= []
+        denominator0= 1/ (np.pow(2*np.pi,xp.shape[1]/2) * np.pow(np.linalg.det(self.sigma),1/2))
+        denominator1= 1/ (np.pow(2*np.pi,xp.shape[1]/2) * np.pow(np.linalg.det(self.sigma),1/2))
+        
+        for i in range(len(xp)):
+            numerator0= np.exp(- (1/2) * (xp[i, :] - self.mean0) @ np.linalg.inv(self.sigma) @ (xp[i, :] - self.mean0).T )
+            numerator1= np.exp(- (1/2) * (xp[i, :] - self.mean1) @ np.linalg.inv(self.sigma) @ (xp[i, :] - self.mean1).T )
+
+            res0.append(numerator0)
+            res1.append(numerator1)
+
+        px_y0= denominator0 * np.array(res0)
+        px_y1= denominator1 * np.array(res1)
+        py0= self.theta
+        py1= 1 - self.theta
+
+        py_x0= px_y0 * py0
+        py_x1= px_y1 * py1
+
+        result= [1 if py_x1[i] > py_x0[i] else 0 for i in range(len(py_x0))]
+
+        return result
